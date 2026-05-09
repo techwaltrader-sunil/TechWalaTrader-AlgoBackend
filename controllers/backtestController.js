785a0e2fe1231b6ae0a746f9dcc4e900541414ff
Priyanka <priyanka2001@gmail.com>
2026-05-09 14:27:52 +0530

    fix(backtest): remove duplicate prefix in Exit All reason string

--- controllers/backtestController.js
+++ controllers/backtestController.js
@@ -6335,7 +6335,7 @@ const runBacktestSimulator = async (req, res) => {
                                 exitTime: currentMinute,
                                 exitPrice: exitP,
                                 pnl: pnl,
-                                exitType: `EXIT_ALL_TRIGGERED_BY_${actualTriggerReason}`
+                                exitType: actualTriggerReason,
                             };
                             
                             dailyBreakdownMap[dateStr].tradesList.push(forcedTrade);
